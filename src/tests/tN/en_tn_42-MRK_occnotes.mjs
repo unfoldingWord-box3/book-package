742b6308d149accc5562b40a1e1fbfad4f59ef52
Cecil New <cecil.new@gmail.com>
2019-11-19 06:41:58 -0500

    ckpt

--- src/tests/tN/en_tn_42-MRK_occnotes.mjs
+++ src/tests/tN/en_tn_42-MRK_occnotes.mjs
@@ -1,6 +1,6 @@
 import { runWordCounts } from '../wordCountsTester.mjs';
 
-const testinput = 'en_tn_42-MRk_occnotes.txt';
+const testinput = 'en_tn_42-MRK_occnotes.txt';
 const ntotal = 769;
 const ndistinct = 222;
 
