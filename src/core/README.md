2cd6c5f49c285b5ce5234de38fb0ee6021001502
mandolyte <cecil.new@gmail.com>
2020-01-31 07:04:53 -0500

    removed langnames - unused

--- src/core/README.md
+++ src/core/README.md
@@ -1,12 +1,1 @@
-## getLanguage = ({languageId})
-
-This non-component function can also have a playground to test it out.
-
-```js
-import {getLanguage} from '../core/langnames.js'
-
-const languageId = "kn";
-const value = getLanguage(languageId);
-
-<span>Language with ID "{languageId}" is {value.ln} ({value.ang})</ span>
-```
+## Nothing to see here...
--- src/core/gitApi.js
+++ src/core/gitApi.js
@@ -4,8 +4,6 @@ import localforage from 'localforage';
 import { setup } from 'axios-cache-adapter';
 import JSZip from 'jszip';
 
-import * as langnames from './langnames';
-
 const baseURL = 'https://git.door43.org/';
 const apiPath = 'api/v1';
 
@@ -70,44 +68,6 @@ export async function fetchResourceManifests({username, languageId}) {
     manifests[resourceId] = manifestArray[index];
   });
   return manifests;
-};
-
-export async function getLanguages({username, resourceIds}) {
-  const languageIds = await getLanguageIds({username, resourceIds});
-  const languages = languageIds.map(languageId =>
-    langnames.getLanguage({languageId})
-  ).filter(language => !!language);
-  languages.sort((a,b) =>
-    (a.languageId > b.languageId) ? 1 : ((b.languageId > a.languageId) ? -1 : 0)
-  );
-  return languages;
-};
-
-export async function getLanguageIds({username, resourceIds}) {
-  let languageIds = [];
-  const promises = resourceIds.map(resourceId => {
-    return getLanguageIdsByResource({username, resourceId});
-  });
-  const languageIdsArray = await Promise.all(promises);
-  const _languageIds = languageIdsArray.flat();
-  _languageIds.forEach(languageId => {
-    const languageAdded = languageIds.includes(languageId);
-    if (!languageAdded) languageIds.push(languageId);
-  });
-  return languageIds;
-}
-
-// /repos/search?q=ulb&uid=4598&limit=50&exclusive=true
-export async function getLanguageIdsByResource({username, resourceId}) {
-  let languageIds = [];
-  const uid = await getUID({username});
-  const params = {q: resourceId, uid, limit: 50, exclusive: true};
-  const uri = Path.join(apiPath, `repos/search`);
-  const repos = await get({uri, params});
-  if (repos && repos.data) {
-    languageIds = repos.data.map(repo => repo.name.split('_')[0]);
-  }
-  return languageIds;
 };
 
 export async function fetchManifest({username, repository}) {
